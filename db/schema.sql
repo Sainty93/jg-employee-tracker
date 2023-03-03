cad8d53de2b715f356e474430fa8f418d4b9b1c5
Sainty93 <jenandfloyd93@icloud.com>
2023-03-03 14:43:44 -0800

    trying to correct functions index.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE company_db;
 
 CREATE TABLE deparment (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-    department_name VARCHAR(30), NOT NULL
+    department_name VARCHAR(30), 
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY, NOT NULL
-    title VARCHAR(30), NOT NULL
-    salary DECIMAL, (10,0) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY 
+    title VARCHAR(30), 
+    salary DECIMAL, (10,0),
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES employee(id)
@@ -19,15 +19,15 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY, NOT NULL
-    first_name VARCHAR(30), NOT NULL
-    last_name VARCHAR(30), NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role (id)
     ON DELETE SET NULL,
-    manager_id INT
+    manager_id INT,
     FOREIGN KEY (manager_id)
-    REFERENCES manager (id)
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const mainMenu = () => {
     return inquirer
     .prompt([
         {
-            type; 'list',
+            type: 'list',
             name: 'main_menu',
             message: 'What can I do for you today?',
             choices: [
@@ -63,13 +63,37 @@ const mainMenu = () => {
                         updateEmployeeRole();
                         break;
                     
+                    case 'Update employee manager':
+                        updateEmployeeManager();
+                        break;
+
+                    case 'View employee by department':
+                        viewEmployeeByDepartment();
+                        break;
+                    
+                    case 'View employee by manager':
+                        viewAllDepartmentsByManager();
+                        break;
+
+                    case 'View total budget by department':
+                        viewDepartmentBudget();
+                        break;
+
                     case 'Quit':
-                        databaseConnection.end();
+                        database.end();
                         break;
             }
         });
 
     };
+
+    const addEmployee = () => {};
+    const addRole = () => {};
+    const updateEmployeeRole = () => {};
+    const updateEmployeeManager = () => {};
+    const viewEmployeeByDepartment = () => {};
+    const viewAllEmployeeByManager = () => {};
+    const viewDepartmentBudget = () => {};
 
     const viewAllDepartments = () => {
         console.log('Viewing all departments...\n');
@@ -86,13 +110,13 @@ const mainMenu = () => {
     const viewAllEmployees = () => {
         console.log('Viewing all employees...\n');
 
-        const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title,
-        department.name AS department, role.salary, Contact(manager.fist_name, '', manager.last_name) AS manager
-        FROM employee
-        LEFT JOIN employee manager on manager.id = employee.manager_id
-        INNER JOIN ROLE on (role.id = employee.role_id)
-        INNER JOIN department on (department.id  = role.department_id)
-        ORDER BY employee.id;`;
+       // const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title,
+       // department.name AS department, role.salary, Contact(manager.fist_name, '', manager.last_name) AS manager
+    //    FROM employee
+      //  LEFT JOIN employee manager on manager.id = employee.manager_id
+       // INNER JOIN ROLE on (role.id = employee.role_id)
+       // INNER JOIN department on (department.id  = role.department_id)
+       // ORDER BY employee.id;`
 
         database.query(query, (err, res) => {
             if (err) throw err;
@@ -104,7 +128,7 @@ const mainMenu = () => {
     const viewAllRoles = () => {
         console.log('Viewing all employees...\n');
 
-        const query = `SELECT role.title, role.id, role.department_id, role.salary;`;
+        const query = `SELECT role.id, role.title, role.department_id, role.salary FROM role;`;
 
         database.query(query, (err, res) => {
             if (err) throw err;
@@ -112,9 +136,9 @@ const mainMenu = () => {
 
             inquirer.prompt([
                 {
-                    type: 'list',
+                    type: 'input',
                     name: 'department',
-                    choices: ['Sales', 'Engineering', 'Finance', 'Customer Service'],
+                    choices: ['What department would you like to add?'],
                 },
             ])
             .then((response) => {
